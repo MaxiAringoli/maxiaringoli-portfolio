4d432a514627736f98d847f16298104c016fdece
Maximiliano Aringoli <maxiaringoli@gmail.com>
2026-09-15 23:21:39 -0300

    feat: track portfolio page views

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { trackEvent } from './analytics'
 import './styles.css'
+
+trackEvent('page_view')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
